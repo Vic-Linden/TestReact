040b13e7a3e353678a10066e6f2c7d3aed5f9470
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-04 20:15:18 +0100

    feat: add darkmode using boolean state

--- testReact/src/components/UserCard/userCard.jsx
+++ testReact/src/components/UserCard/userCard.jsx
@@ -3,18 +3,25 @@ import {useState} from 'react';
 
 function UserCard({name, role}) {
 const [likes, setLikes] = useState(0);
+const [isDark, setIsDark] = useState(false); //false = starts with light mode.
+
 
 const handleLike = () => {
     setLikes (likes + 1)
 };
 
+const lightMode = () => {
+    setIsDark(!isDark);
+};
+
 
 
     return (
-        <div className="user-card">
+        <div className={`user-card ${isDark ? 'dark-mode' : ''}`}> {/*ternary operation*/}
             <h3>{name}</h3>
-            <p>Roll: {role}</p>
+            <p>Role: {role}</p>
             <button onClick={handleLike}>Like: {likes}</button>
+            <button onClick={lightMode}>Change darkmode</button>
         </div>
     );
 }
