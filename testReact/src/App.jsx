190c1d43cef96fc3db8e3d6d55e02f4efb0c7489
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-04 14:07:25 +0100

    refactor: correct folder structure for components

--- testReact/src/App.jsx
+++ testReact/src/App.jsx
@@ -1,15 +1,2 @@
 import Header from './components/Header/header';
-
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <p>här börjar mitt projekt!</p>
-      </main>
-    </div>
-  )
-}
-
-export default App
+import UserCard from './components/UserCard';
