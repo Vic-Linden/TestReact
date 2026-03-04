d1ce50ea71f5674cee1db42d7a5d933abf6b7d3b
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-04 13:34:53 +0100

    first commit

---
+++ testReact/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/header';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>här börjar mitt projekt!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ testReact/src/components/Header/header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header className="main-header">
+            <h2>Min test header</h2>
+        </header>
+    );
+}
+
+export default Header;
