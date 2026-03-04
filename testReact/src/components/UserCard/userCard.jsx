711bdb9674c94ac2863817df3d2f6b85785a9874
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-04 19:34:33 +0100

    feat: practice useState by adding a like counter

--- testReact/src/components/UserCard/userCard.jsx
+++ testReact/src/components/UserCard/userCard.jsx
@@ -1,10 +1,20 @@
 import './UserCard.css'
+import {useState} from 'react';
 
 function UserCard({name, role}) {
+const [likes, setLikes] = useState(0);
+
+const handleLike = () => {
+    setLikes (likes + 1)
+};
+
+
+
     return (
         <div className="user-card">
             <h3>{name}</h3>
             <p>Roll: {role}</p>
+            <button onClick={handleLike}>Like: {likes}</button>
         </div>
     );
 }
