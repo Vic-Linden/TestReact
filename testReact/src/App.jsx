0e6107122f61f0c613fd97dee63995a3f679896c
victoria.linden93@gmail.com <victoria.linden93@gmail.com>
2026-03-04 14:30:57 +0100

    feat: implement usesrCards instances in App

--- testReact/src/App.jsx
+++ testReact/src/App.jsx
@@ -1,2 +1,17 @@
 import Header from './components/Header/header';
-import UserCard from './components/UserCard';
+import UserCard from './components/UserCard/userCard';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main style={{ display: 'flex', gap: '10px', padding: '20px'}}>
+        <UserCard name="aldor" role="teacher" />
+        <UserCard name="johan" role="teacher" />
+        <UserCard name="pelle" role="teacher" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
